1402de7adafbd0d77337ceeaa7925384baf4d9b4
hasnainraza <hr779656@gmail.com>
2022-10-25 17:48:51 +0500

    my first commit

---
+++ about.js
@@ -0,0 +1,37 @@
+function myFunc(){
+    var table_intiger = prompt("PUT IN YOUR TABLE INTIGER");
+    var table_counting = 1;
+    var intiger_multiply = table_intiger * table_counting;
+    var table_counting2 = 2;
+    var intiger_multiply2 = table_intiger * table_counting2;
+    var table_counting3 = 3;
+    var intiger_multiply3 = table_intiger * table_counting3;
+    var table_counting4 = 4;
+    var intiger_multiply4 = table_intiger * table_counting4;
+    var table_counting5 = 5;
+    var intiger_multiply5 = table_intiger * table_counting5;
+    var table_counting6 = 6;
+    var intiger_multiply6 = table_intiger * table_counting6;
+    var table_counting7 = 7;
+    var intiger_multiply7 = table_intiger * table_counting7;
+    var table_counting8 = 8;
+    var intiger_multiply8 = table_intiger * table_counting8;
+    var table_counting9 = 9;
+    var intiger_multiply9 = table_intiger * table_counting9;
+    var table_counting10 = 10;
+    var intiger_multiply10 = table_intiger * table_counting10;
+    document.write("<h1>HERE IS YOUR TABLE SIR</h1>")
+    document.write("<br><br>")
+    document.write(`${table_intiger} × ${table_counting} = ${intiger_multiply} <br>`)
+    document .write(`${table_intiger} × ${table_counting2} = ${intiger_multiply2}<br>`)
+    document .write(`${table_intiger} × ${table_counting3} = ${intiger_multiply3}<br>`)
+    document .write(`${table_intiger} × ${table_counting4} = ${intiger_multiply4}<br>`)
+    document .write(`${table_intiger} × ${table_counting5} = ${intiger_multiply5}<br>`)
+    document .write(`${table_intiger} × ${table_counting6} = ${intiger_multiply6}<br>`)
+    document .write(`${table_intiger} × ${table_counting7} = ${intiger_multiply7}<br>`)
+    document .write(`${table_intiger} × ${table_counting8} = ${intiger_multiply8}<br>`)
+    document .write(`${table_intiger} × ${table_counting9} = ${intiger_multiply9}<br>`)
+    document .write(`${table_intiger} × ${table_counting10} = ${intiger_multiply10}<br>`)
+}
+
+myFunc()
